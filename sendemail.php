35c51e8d3265d828ccb1cf727f0147b47cc402d2
Hizkiel Alemayehu <44091398+Hizkie@users.noreply.github.com>
2020-12-22 20:12:10 +0100

    Update sendemail.php

--- sendemail.php
+++ sendemail.php
@@ -1,34 +1,40 @@
 <?php
+// Import PHPMailer classes into the global namespace
+// These must be at the top of your script, not inside a function
 use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
-require_once "vendor/autoload.php";
+// Load Composer's autoloader
+require 'vendor/autoload.php';
 
-//PHPMailer Object
-$mail = new PHPMailer(true); //Argument true in constructor enables exceptions
-
-//From email address and name
-$mail->From = "hizclick@gmail.com";
-$mail->FromName = "Full Name";
-
-//To address and name
-$mail->addAddress("hizclick@gmail.com", "Recepient Name");
-$mail->addAddress("hizclick@gmail.com"); //Recipient name is optional
-
-//Address to which recipient will reply
-$mail->addReplyTo("hizclick@gmail.com", "Reply");
-
-
-//Send HTML or Plain Text email
-$mail->isHTML(true);
-
-$mail->Subject = "Subject Text";
-$mail->Body = "<i>Mail body in HTML</i>";
-$mail->AltBody = "This is the plain text version of the email content";
+// Instantiation and passing `true` enables exceptions
+$mail = new PHPMailer(true);
 
 try {
+    //Server settings
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
+    $mail->isSMTP();                                            // Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+    $mail->Username   = 'hizclick@gmail.com';                     // SMTP username
+    $mail->Password   = 'secret';                               // SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+    $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+
+    //Recipients
+    $mail->setFrom('hizclick@gmail.com', 'Mailer');
+    $mail->addAddress('hizclick@gmail.net', 'Joe User');     // Add a recipient
+    $mail->addReplyTo('hizclick@gmail.com', 'Information');
+
+  // Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = 'Here is the subject';
+    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
     $mail->send();
-    echo "Message has been sent successfully";
+    echo 'Message has been sent';
 } catch (Exception $e) {
-    echo "Mailer Error: " . $mail->ErrorInfo;
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
 }
