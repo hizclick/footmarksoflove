878cbba1b2419c6ad69c14a0a5741f7192d18118
Hizkiel Alemayehu <44091398+Hizkie@users.noreply.github.com>
2020-12-22 21:00:40 +0100

    Update sendemail.php

--- sendemail.php
+++ sendemail.php
@@ -1,39 +1,18 @@
 <?php
-// Import PHPMailer classes into the global namespace
-// These must be at the top of your script, not inside a function
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\SMTP;
-use PHPMailer\PHPMailer\Exception;
 
-// Load Composer's autoloader
+use PHPMailer\PHPMailer\PHPMailer;
 require 'vendor/autoload.php';
 
-// Instantiation and passing `true` enables exceptions
-$mail = new PHPMailer(true);
+$mail = new PHPMailer();
+$mail->setFrom('hizclick@gmail.com', 'First Last');
+$mail->addAddress('hizclick@gmail.com', 'John Doe');
+$mail->Subject = 'Say Hello to HELO';
+$mail->Body = '<h1>Hi!</h1><p>This is my HTML body</p>'
+$mail->AltBody = 'This is a plain-text message body';
 
-try {
-    //Server settings
-    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
-    $mail->isSMTP();                                            // Send using SMTP
-    $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
-    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-    $mail->Username   = 'hizclick@gmail.com';                     // SMTP username
-    $mail->Password   = 'hizclickhepta';                               // SMTP password
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
-    $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
-
-    //Recipients
-    $mail->setFrom('hizclick@gmail.com', 'Mailer');
-
-    $mail->addAddress('hizclick@gmail.com', 'Joe User'); 
-  // Content
-    $mail->isHTML(true);                                  // Set email format to HTML
-    $mail->Subject = 'Here is the subject';
-    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-
-    $mail->send();
-    echo 'Message has been sent';
-} catch (Exception $e) {
-    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+if (!$mail->send()) {
+    echo 'Mailer Error: ' . $mail->ErrorInfo;
+} else {
+    echo 'Message sent!';
 }
+?>
